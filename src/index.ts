17d64e661748369624deff5853710736ff9a2c76
Jack Le Hamster <jacklehamster@gmail.com>
2024-02-17 15:09:10 -0800

    Auto-commit: 2024-02-17 15:09:10
    Files:
    src/index.ts
    src/types/UpdateType.ts
    
    https://github.com/jacklehamster/autocommit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { dist, equal } from "./utils/vector-utils";
 import { VectorPool } from "./pools/VectorPool";
 import { Active } from "./types/Active";
 import { Val } from "./types/Val";
+import { UpdateType } from "./types/UpdateType";
 
-export type { IdType, Vector, Active, Val };
+export type { IdType, Vector, Active, Val, UpdateType };
 export { dist, equal, VectorPool };
